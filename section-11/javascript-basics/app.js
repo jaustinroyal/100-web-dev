8408f68b675e77c18a25d79e05eb6ebf5a270ff1
jaustin2010 <jamesaustin232010@gmail.com>
2022-06-29 15:08:56 +0530

    completed section-11 learned javascript

---
+++ section-11/javascript-basics/app.js
@@ -0,0 +1,8 @@
+let name = "James Austin";
+let age = 14;
+let hobbies = ["cycling", "badminton(in progress)", "etc ;P"];
+let job = { title: "Developer", place: "Unknown", salary: "Infinite" };
+
+alert(name);
+alert(job["title"]);
+alert(hobbies[2]);
